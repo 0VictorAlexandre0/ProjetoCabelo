44fdff72b5425d95d4695c40d78d60eb4dcb5576
Victor <00victoralexandre00@gmail.com>
2026-07-31 14:57:27 -0300

    Configurações iniciais

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = "postgresql://adm:1234@localhost/bdprojeto"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    "bando de dados usado"
+    "postgresql://          adm      :1234   @localhost  /bdprojeto"
+    "postgresql://          usuario  :senha  @servidor   /nome_do_banco"
+    
